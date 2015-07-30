73d68479f70dda3fc9cbc8a300702df41e1c7c1c
Aaron Cripps <acripps@gmail.com>
2015-07-29 21:55:27 -0230

    use @staticmethod decorator instead of calling staticmethod() function

--- storage/StorageFactory.py
+++ storage/StorageFactory.py
@@ -2,8 +2,8 @@ from DictStorage import DictStorage
 
 class StorageFactory:
     factories = {}
+    @staticmethod
     def createStorage(id, *args, **kwargs):
         if not StorageFactory.factories.has_key(id):
             StorageFactory.factories[id] = eval(id + '.Factory()')
         return StorageFactory.factories[id].create(args, kwargs)
-    createStorage = staticmethod(createStorage)
